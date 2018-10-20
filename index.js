23eab54ccd144141c370fb3f2afd1c366d98bf06
Alex Ruiz <ruizaa276@gmail.com>
2018-10-20 18:34:54 -0500

    Clean up

--- index.js
+++
@@ -1,9 +0,0 @@
-var mainText = document.getElementById("mainText");
-var submitButton = document.getElementById("submitButton");
-
-function submitClick(){
-  window.alert("Test test test ");
-  var firebaseRef = firebase.databse().ref();
-
-  firebaseRef.child("text").set("value");
-}
