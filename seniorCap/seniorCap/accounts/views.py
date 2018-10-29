31c9017981546bcd8b5b4a8716d5ce1e7221f4d3
Alex Ruiz <ruizaa276@gmail.com>
2018-10-29 17:49:08 -0500

    New Apps added

--- seniorCap/seniorCap/accounts/views.py
+++ seniorCap/seniorCap/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'accounts/home.html')
+
+def about(request):
+    return HttpResponse(request, 'accounts/about.html')
