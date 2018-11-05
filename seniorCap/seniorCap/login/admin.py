ed11c1312c10a9cb66bdbbb5301fa65669e98bf2
Alex Ruiz <ruizaa276@gmail.com>
2018-11-05 17:24:53 -0600

    Models and data~

--- seniorCap/seniorCap/login/admin.py
+++ seniorCap/seniorCap/login/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Faculty
+from .models import Student
+from .models import Skills
 # Register your models here.
+
+admin.site.register(Faculty)
+admin.site.register(Student)
+admin.site.register(Skills)
--- seniorCap/seniorCap/login/models.py
+++ seniorCap/seniorCap/login/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Extension of the User Object
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.user.username
+
+class Faculty(models.Model):
+	faculty_id = models.IntegerField()
+	faculty_name_first = models.CharField(max_length=50)
+	faculty_name_last = models.CharField(max_length=50)
+	faculty_email = models.CharField(max_length=50)
+	def __str__(self):
+		return self.faculty_name_last
+
+class Student(models.Model):
+	student_id = models.IntegerField()
+	student_name_first = models.CharField(max_length=50)
+	student_name_last = models.CharField(max_length=50)
+	student_email = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.student_name_last
+
+class Skills(models.Model):
+    tech_Stack = models.CharField(max_length=50)
+    gen_skills = models.CharField(max_length=50)
+    major = models.CharField(max_length=50)
+    tech_Stack = models.CharField(max_length=50)
